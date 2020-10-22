495087a7937b6f1eb832a2e8377be4bee7ccea6a
POTATIIS <alexander.martinsson@elev.ga.ntig.se>
2020-10-22 10:36:26 +0200

    Initial commit

---
+++ Firewall.ps1
@@ -0,0 +1,6 @@
+﻿$portar = import-csv -delimiter ";" -path "C:\code\powershell\firewall\port.csv"
+
+foreach ($port in $portar) {
+    New-NetFirewallRule -DisplayName $port.protokoll -Direction Outbound -Protocol TCP -LocalPort $port.port -Action $port.regel
+    write-host "Porten" $port.port "är nu" $port.regel
+}
